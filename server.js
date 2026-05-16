5cb12b6773737b7e6d4c5a7c2794e1fc8a2f372c
Allan Domingos <allan.andrade.professor@gmail.com>
2026-05-16 11:58:23 -0300

    primeiro commit backend

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/saudacao', (req, res) => {
+
+    const { nome } = req.body;
+
+    res.json({
+        mensagem: `Olá ${nome}!`
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
